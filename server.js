e5af48234f6aa0f5ae65d395d677e9a62ef0e685
Cezar Mocan <mocancezar@gmail.com>
2016-01-31 21:10:38 -0500

    Serving empty page works

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+require('babel-register')
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser  = require('body-parser');
 var pg = require('pg');
+
+var swig  = require('swig');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var Router = require('react-router');
+var routes = require('./app/routes');
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +19,21 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
+app.use(function(req, res) {
+  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirectLocation) {
+      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+      var page = swig.renderFile('views/index.html', { html: html });
+      res.status(200).send(page);
+    } else {
+      res.status(404).send('Page Not Found')
+    }
+  });
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
