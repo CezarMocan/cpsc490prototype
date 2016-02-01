93fc3107ae2f8cf43dd35fa67bcce6269fd99557
Cezar Mocan <mocancezar@gmail.com>
2016-01-31 19:29:47 -0500

    Server running

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var path = require('path');
 var bodyParser  = require('body-parser');
 var pg = require('pg');
 
-app.set('port', (process.env.PORT || 5050));
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
